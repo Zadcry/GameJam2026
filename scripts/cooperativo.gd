ab266fd4305f8c83915170f432f3945c89abd5bb
AncientPie <camiloarcgu@unisabana.edu.co>
2026-06-07 14:11:24 -0500

    Add players logo in Main (tutorial) and add label "OrNot" in coop.tscn

--- scripts/cooperativo.gd
+++ scripts/cooperativo.gd
@@ -3,9 +3,11 @@ extends Node
 @onready var coop := $coop
 @onready var sprite_e := $E
 @onready var sprite_flecha := $flecha
+@onready var OrNot := $OrNot
 
 func _ready() -> void:
 	FadeManager.fade_in()
+	OrNot.modulate.a = 0.0
 	coop.modulate.a = 0.0
 	sprite_e.visible = false
 	sprite_flecha.visible = false
@@ -13,7 +15,9 @@ func _ready() -> void:
 
 func _animar() -> void:
 	var tween = create_tween()
+	tween.set_parallel(true)
 	tween.tween_property(coop, "modulate:a", 1.0, 2.0)
+	tween.tween_property(OrNot, "modulate:a", 0.3, 2.0)
 	await tween.finished
 	sprite_e.visible = true
 	sprite_flecha.visible = true
@@ -21,6 +25,7 @@ func _animar() -> void:
 func _input(event: InputEvent) -> void:
 	if sprite_e.visible and event.is_action_pressed("p1_shoot"):
 		coop.visible = false
+		OrNot.visible = false
 		sprite_e.visible = false
 		sprite_flecha.visible = false
 		await FadeManager.fade_out()
